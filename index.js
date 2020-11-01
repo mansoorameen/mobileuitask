149383605d3a9b4b937df8237676fad68e8f3400
mansoorameen <manzooravail@gmail.com>
2020-11-01 17:01:32 +0530

    codes

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function pageRedirect(){
+    var delay = 2000; 
+   
+    
+    document.getElementById("changes");
+    
+    setTimeout(function(){
+     window.location = "home.html";
+    },delay);
+    
+   }
+   pageRedirect();
+
+   function loginpage() {
+    window.location = "login.html";
+   }
